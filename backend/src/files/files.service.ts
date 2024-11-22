9777ca309c9e8bb619d3d10621fc9cec1d89714c
Nikita <natrokhov91@gmail.com>
2024-11-22 10:27:50 +0700

    refactoring

--- backend/src/files/files.service.ts
+++ backend/src/files/files.service.ts
@@ -2,13 +2,13 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { Product } from '@prisma/client';
 import { promises as fs } from 'fs';
 import { join } from 'path';
-import { ProductService } from 'src/product/product.service';
+import { ProductService } from '../product/product.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class FilesService {
+  constructor(private prisma: PrismaService) {}
   private readonly uploadPath = join(__dirname, '..', '..', 'uploads');
-
-  constructor(private readonly productService: ProductService) {}
 
   async uploadFile(id: Product['id'], file: Express.Multer.File) {
     if (!file) {
@@ -30,8 +30,13 @@ export class FilesService {
       lastDotIndex !== -1 ? file.originalname.substring(lastDotIndex) : '';
     const uploadFileName = `${Date.now()}${postfix}`;
 
-    await this.productService.updateProduct(id, {
-      photoPath: uploadFileName,
+    // await this.productService.updateProduct(id, {
+    //   photoPath: uploadFileName,
+    // });
+
+   await this.prisma.product.update({
+      where: { id },
+      data: {  photoPath: uploadFileName },
     });
 
     await fs.mkdir(this.uploadPath, { recursive: true });
